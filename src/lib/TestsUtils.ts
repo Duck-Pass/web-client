d525e526dfac26cc0ed81d39e740081b68e74eea
azzen <6373020+azzen@users.noreply.github.com>
2023-08-28 21:36:03 +0200

    test: add utilities function for tests

---
+++ src/lib/TestsUtils.ts
@@ -0,0 +1,7 @@
+export function makeByteArray(length: number, start = 0): Uint8Array {
+    const arr = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+        arr[i] = start + i;
+    }
+    return arr;
+}
