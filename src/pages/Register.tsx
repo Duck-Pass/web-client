ba3b3f7e97f67d0f41f4f16a7a2f35b410839afc
Hugo <mrjokesheep@gmail.com>
2023-08-28 11:58:25 +0200

    feat: add validation to register form and modify form page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,50 +1,37 @@
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import { UserAuthForm } from "@/components/auth/user-auth-form"
 import { Link } from "react-router-dom"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import Logo from "@/assets/ducky-round.png"
+import { RegisterAuthForm } from "@/components/auth/register-auth-form"
 
-export default function AuthenticationPage() {
+export default function Register() {
   return (
     <>
-      <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          to="/login"
-          className={cn(
-            buttonVariants({ variant: "ghost" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
+      <div className="container h-screen flex flex-col items-center justify-center">
+        <Link to="/">
+          <img className="w-32 h-32 p-2 -mt-10 mb-2 rounded-full" src={Logo} alt="" />
         </Link>
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-          <div className="absolute inset-0 bg-zinc-900" />
-          <div className="relative z-20 flex items-center text-lg font-medium">
-            <img className="w-12 h-12 m-3" src={Logo} alt="DuckPass Logo" />
-            DuckPass
-          </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;Security must not be a choice.&rdquo;
-              </p>
-              <footer className="text-sm">Alexis Martins, CEO</footer>
-            </blockquote>
-          </div>
-        </div>
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
-            </div>
-            <UserAuthForm />
-          </div>
-        </div>
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Register</CardTitle>
+            <CardDescription>Create your account to access your personnal vault.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <RegisterAuthForm />
+          </CardContent>
+          <CardFooter className="flex flex-col">
+            <p className="text-xs text-center text-gray-700">
+              Already have an account? {" "}
+              <Link to="/login" className=" text-blue-600 hover:underline">Sign in</Link>
+            </p>
+          </CardFooter>
+        </Card>
       </div>
     </>
   )
