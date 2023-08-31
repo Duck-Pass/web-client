74b08da309f74db873a88bb21fcedf67b94f18b8
azzen <6373020+azzen@users.noreply.github.com>
2023-08-31 15:36:43 +0200

    feat(vault): add vault manager

---
+++ src/lib/models/vault.ts
@@ -0,0 +1,114 @@
+export type Credential = {
+    id: string,
+    name: string
+    username: string
+    password: string
+    authKey: string
+    totp: string
+    note: string
+    favorite?: boolean
+}
+
+import { BufferUtils } from '../BufferUtils'
+import { WebCryptoEncryptionService } from '../services/webcrypto-encryption.service'
+import { WebCryptoPrimitivesService } from '../services/webcrypto-primitives.service'
+import { SymmetricCryptoKey, UserKey } from './symmetric-crypto-key'
+import { v4 as uuid } from 'uuid';
+
+type Vault = Credential[]
+
+export class VaultManager {
+    private static vault: Vault
+    private static instance : VaultManager
+    private static key: UserKey | null
+    private constructor() {}
+
+    public static getInstance(): VaultManager {
+        if (!VaultManager.instance) {
+            VaultManager.instance = new VaultManager()
+
+            const vaultJson = localStorage.getItem('vault')
+            const userProfile = JSON.parse(localStorage.getItem('userProfile') ?? "");
+            const userKey = userProfile.symmetric_key?.b64key ?? "";
+            if (userKey) {
+                VaultManager.key = new SymmetricCryptoKey(BufferUtils.fromBase64ToByteArray(userKey)) as UserKey;
+            }
+            if (vaultJson) {
+                VaultManager.vault = JSON.parse(vaultJson)
+            }
+
+            if (!VaultManager.vault) {
+                VaultManager.vault = []
+            }
+        }
+        return VaultManager.instance
+    }
+
+    public async clear() {
+        VaultManager.vault = []
+        VaultManager.key = null
+        localStorage.removeItem('vault')
+
+    }
+
+    public async lock() {
+        let jsonVault = JSON.stringify(VaultManager.vault)
+        if (jsonVault && VaultManager.key) {
+            const primitives = new WebCryptoPrimitivesService(window);
+            const encryptionService = new WebCryptoEncryptionService(primitives);
+            const encryptedVault = await encryptionService.encrypt(jsonVault, VaultManager.key);
+            localStorage.setItem('vault', encryptedVault.toJSON());
+        }
+
+    }
+
+    public getVault(): Vault {
+        return VaultManager.vault
+    }
+
+    public async editItem(item: Credential) {
+        VaultManager.vault = VaultManager.vault.map((i) => {
+            if (i.id === item.id) {
+                return item
+            }
+            return i
+        });
+    }
+
+    public async addItem(item: Credential) {
+        item.id = uuid()
+        VaultManager.vault.push(item)
+        await this.sync()
+    }
+
+    public async removeItem(id: string) {
+        VaultManager.vault = VaultManager.vault.filter((i) => i.id !== id)
+        await this.sync()
+    }
+
+    public async sync() {
+        let jsonVault = JSON.stringify(VaultManager.vault)
+        if (jsonVault && VaultManager.key) {
+            const primitives = new WebCryptoPrimitivesService(window);
+            const encryptionService = new WebCryptoEncryptionService(primitives);
+            const encryptedVault = await encryptionService.encrypt(jsonVault, VaultManager.key);
+            await fetch('https://api-staging.duckpass.ch/update_vault', {
+                method: 'POST',
+                body: JSON.stringify({
+                    vault: encryptedVault.toJSON()
+                }),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+            })
+            localStorage.setItem('vault', jsonVault);
+        }
+    }
+
+    public scheduleLock(timeout: number) {
+        setTimeout(() => {
+            this.lock()
+        }, timeout)
+    }
+}
