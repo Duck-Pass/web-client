43b1aedbdc762066cf2086876377fcc564c0b898
azzen <6373020+azzen@users.noreply.github.com>
2023-09-01 23:33:54 +0200

    feat(vault): add vault context

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Vault from './pages/Vault'
 import AccountVerified from './pages/AccountVerified';
 import Profile from './pages/Profile'
 import { AuthContextProvider } from './components/context/AuthContextProvider';
+import { VaultContextProvider } from './components/context/VaultContextProvider';
 import PasswordReset from './pages/PasswordReset';
 import Login2FA from './pages/Login2FA';
 import Page404 from './pages/Page404'
@@ -24,7 +25,11 @@ function App() {
           <Route path="/reset-password" element={<PasswordReset />} />
           <Route path="/2fa-login" element={<Login2FA />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/vault" element={<Vault />} />
+            <Route path="/vault" element={
+            <VaultContextProvider>
+              <Vault />
+            </VaultContextProvider>
+            } />
           <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
